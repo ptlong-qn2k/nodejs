4029d1802116b8e28850ea43455a6f0f7f275c9a
ptlong-qn2k <thanhlong05.06.2k@gmail.com>
2024-03-18 10:30:59 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-
+import Express  from 'express'
+const path =require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname,'/index.html'))
 })
 app.get('/about',(req,res)=>{
   res.send(`I'm thanh long`)
