f6e6ef908d8c3ba2f181483279b7a5b2433b9b3c
ptlong-qn2k <thanhlong05.06.2k@gmail.com>
2024-03-20 17:47:23 +0700

    router params

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,23 +1,13 @@
-// Get the client
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 // Create the connection to database
-const connection = mysql.createConnection({
+console.log("creating connectin pool...");
+
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'nodejsbasic',
+    // password:'password'
 });
 
-// // A simple SELECT query
-// connection.query(
-//   'SELECT * FROM `users` ',
-//   function (err, results, fields) {
-//     console.log('>>>check mysql');
-//     console.log(results); // results contains rows returned by server
-//     let rows = results.map((row)=>{return row});
-//     console.log(rows); // results contains rows returned by server
-
-//     // console.log(fields); // fields contains extra meta data about results, if available
-//   }
-// );
-export default connection
+export default pool
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,27 +1,19 @@
-import connection from "../configs/connectDB"
-let getHomepage = (req, res) => {
-    //logic
-    let data = [];
-    connection.query(
-        'SELECT * FROM `users` ',
-        function(err, results, fields) {
-            console.log('>>>check mysql');
-            console.log(results); // results contains rows returned by server
-            results.map((row) => {
-                data.push({
-                    id: row.id,
-                    email: row.email,
-                    address: row.address,
-                    firstName: row.firstName,
-                    lastName: row.lastName
-                })
-            });
-            console.log('>>>checkdata', typeof(data), JSON.stringify(data));
-            return res.render("index.ejs", { dataUser: data, test: "string abcs" })
+import pool from "../configs/connectDB"
 
-        });
+let getHomepage = async(req, res) => {
+    const [rows, fields] = await pool.execute('SELECT * FROM `users` ');
+    // let check = await pool.execute('SELECT * FROM `users` ');
+    // console.log(check[0]);
+    return res.render("index.ejs", { dataUser: rows, test: "string abcs" })
+}
 
+let getDetailPage = async(req, res) => {
+    let userid = req.params.id;
+    let user = await pool.execute('select * from users where id = ? And firstName = ?', [userid, 'Pham']);
+    // console.log('check request params', user)
+    return res.send(JSON.stringify(user[0]))
 }
 module.exports = {
-    getHomepage
+    getHomepage,
+    getDetailPage
 }
