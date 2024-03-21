0eb263909f2b565f601bb73e4754e2ef2836e544
ptlong-qn2k <thanhlong05.06.2k@gmail.com>
2024-03-21 16:57:23 +0700

    form $ post request

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -13,7 +13,18 @@ let getDetailPage = async(req, res) => {
     // console.log('check request params', user)
     return res.send(JSON.stringify(user[0]))
 }
+let createNewUser = async(req, res) => {
+    console.log('>>check req:', req.body);
+    // let { firstName, lastName, email, address } = req.body
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName
+    let email = req.body.email
+    let address = req.body.address
+    await pool.execute('insert into users(firstName, lastName, email,address) VALUES(?,?,?,?)', [firstName, lastName, email, address])
+    return res.redirect('/') //post du lieu xong ve trang home
+}
 module.exports = {
     getHomepage,
-    getDetailPage
+    getDetailPage,
+    createNewUser
 }
