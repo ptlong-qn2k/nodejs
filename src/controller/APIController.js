0493c9befb8db28aa6f95fff0e4c8b9a12c2ae94
ptlong-qn2k <106219575+ptlong-qn2k@users.noreply.github.com>
2024-03-24 20:34:45 +0700

    Update APIController.js

--- src/controller/APIController.js
+++ src/controller/APIController.js
@@ -21,13 +21,13 @@ let createNewUser = async(req, res) => {
     })
 }
 let updateUser = async(req, res) => {
-    let { firstName, lastName, email, address } = req.body
+    let { firstName, lastName, email, address, id } = req.body
     if (!firstName || !lastName || !email || !address) {
         return res.status(200).json({
             message: 'missing required params'
         })
     }
-    await pool.execute('insert into users(firstName, lastName, email,address) VALUES(?,?,?,?)', [firstName, lastName, email, address])
+    await pool.execute('update users set firstName=?, lastName=?, email=?,address=? where id=?', [firstName, lastName, email, address, id])
 
     return res.status(200).json({
         massage: 'ok'
